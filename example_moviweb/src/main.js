ca11b1a9a98bb2550e1a9a0ee1dff16d5366e46d
luis d martinez <luis d martinez@FYRMN32>
2019-11-12 09:23:25 -0400

    initial commit

---
+++ example_moviweb/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import axios from 'axios'
+
+Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json'
+new Vue({
+  vuetify,
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
